3c2f5e73df7662d22fd428737625164a6b2a2d4e
jeromeNoir <45006978+jeromeNoir@users.noreply.github.com>
2025-04-12 16:53:10 +0200

    Update openPIV_CodeExample.py

--- openPIV_CodeExample.py
+++ openPIV_CodeExample.py
@@ -1,18 +1,50 @@
-'''
-To use the code
-python3 openPIV <path to parameter file>
+"""
+Script to process Particle Image Velocimetry (PIV) images.
 
-input_folder: '<Images Folder>/'
-output_folder: '<Results Folder>/'
-extension: 'tif' extension of the images to be processed ('jpg', 'png', 'tif', etc.)
+Usage:
+    python openPIV_codeExammple.py <parameterFile>
 
+Arguments:
+    parameterFile (str): Path to the YAML file containing PIV processing parameters.
 
-The code processes the Images without preprocessing, 
-save the results and plots the first PIV field.
+Description:
+    This script processes PIV images using the `openpiv` library. It reads parameters 
+    from a YAML configuration file, performs PIV analysis on pairs of images, and 
+    optionally applies post-processing, scaling, and coordinate transformations. 
+    The results are saved as text files in the specified output directory.
 
-This Code is based on the openPIV code 
-'''
+Steps:
+    1. Prepare a YAML file with the required parameters (e.g., input/output folders, 
+       PIV settings, post-processing options).
+    2. Place the images to be processed in the input folder specified in the YAML file.
+    3. Run the script with the path to the YAML file as an argument.
 
+Example:
+    python openPIV_codeExammple.py parameters.yaml
+
+YAML File Example:
+    input_folder: "./input_images/"
+    output_folder: "./output_results/"
+    extension: ".png"
+    isPostProcess: True
+    threshold_sigNoise: 1.2
+    kernel_size: 5
+    isScaling: True
+    scaling_factor: 96.52
+    dtPIV: 1
+    isProcessAll: True
+    step: 1
+    window_sizePIV: 48
+    search_area_sizePIV: 64
+    overlapPIV: 24
+
+Requirements:
+    - Python 3.x
+    - Libraries: numpy, openpiv, PIL, PyYAML
+
+Output:
+    - Processed PIV vector fields saved in the output folder as text files.
+"""
 
 #Standar libraries
 import numpy as np
